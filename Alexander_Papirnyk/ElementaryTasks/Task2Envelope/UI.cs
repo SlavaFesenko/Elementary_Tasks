c80a425f8c6c3cf7130079d3b85c0a95d80b8231
Alexander Papirnyk <gennerys1515@gmail.com>
2019-06-04 12:26:39 +0300

    init commit
    added tasks

---
+++ Alexander_Papirnyk/ElementaryTasks/Task2Envelope/UI.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2Envelope
+{
+
+    public static class UI
+    {        
+        public static string InputFromConsole()
+        {
+            return Console.ReadLine();
+        }
+
+        public static void ShowMessage(string message)
+        {
+             Console.WriteLine(message); 
+        }
+    }
+}
---
+++ Alexander_Papirnyk/ElementaryTasks/Task2Envelope/Validator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2Envelope
+{
+    public  static class Validator
+    {
+        public static bool Input(string first, string second,string third,string forth, out double [] mas)
+        {
+            mas = new double[4];
+            bool isFirstEnvelopeHeightChecked = double.TryParse(first, out mas[0]);
+            bool isFirstEnvelopeWidthChecked = double.TryParse(second, out mas[1]);
+            bool isSecondEnvelopeHeightChecked = double.TryParse(third, out mas[2]);
+            bool isSecondEnvelopeWidthChecked = double.TryParse(forth, out mas[3]);
+
+            if (isFirstEnvelopeHeightChecked && isFirstEnvelopeWidthChecked &&
+                isSecondEnvelopeHeightChecked && isSecondEnvelopeWidthChecked &&
+                mas[0] > 0.0 && mas[1] > 0.0 &&
+                mas[2] > 0.0 && mas[3] > 0.0)
+            { return true; }
+            else
+            {
+                return false;
+            }
+
+        }
+
+
+
+
+    }
+}
